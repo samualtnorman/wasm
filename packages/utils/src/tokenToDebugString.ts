4ae0cd2b4b45557ce0a6c714923068d993fd80a6
Samual <me@samual.uk>
2024-10-09 22:58:03 +0100

    use `TokenTagsToNames` from `TokenTag.ts`

--- packages/utils/src/tokenToDebugString.ts
+++ packages/utils/src/tokenToDebugString.ts
@@ -1,7 +1,5 @@
 import type { Token } from "./tokenise"
-import { TokenTag } from "./TokenTag"
-
-const TokenTagsToNames = Object.fromEntries(Object.entries(TokenTag).map(([ name, tag ]) => [ tag, name ]))
+import { TokenTagsToNames } from "./TokenTag"
 
 export const tokenToDebugString = (token: Token, code: string) =>
 	`${TokenTagsToNames[token.tag]} ${JSON.stringify(code.slice(token.index, token.index + token.size))} ${token.index}`
